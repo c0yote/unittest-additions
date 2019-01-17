0bf8f22e344f02d21618fd0f3619f6eb1914266f
U.G. Wilson <ugwilson@gmail.com>
2019-01-17 09:44:03 -0600

    Adding other useful stuff.

--- unittest_additions.py
+++ unittest_additions.py
@@ -1,3 +1,10 @@
+import os
+
+def fix_iteration_in_mock_open(open_mock):
+  """ A test utility function to help with file open mocking.
+  """
+  open_mock.return_value.__iter__ = lambda self : iter(self.readline, '')
+
 
 class AdditionalAssertsMixin:
     def __init__(self, *args, **kwargs):
@@ -12,4 +19,25 @@ class AdditionalAssertsMixin:
         if len(collection) < 1:
             msg = self._formatMessage(msg, f'{type(collection).__name__} is empty.')
             raise self.failureException(msg)
-    
+
+
+class TemporaryFile:
+    def __init__(self, filepath, content):
+        if os.path.exists(filepath):
+            raise AssertionError('Temporary file target already exists: ' + filepath)
+        self.filepath = filepath
+        self._initial_content = content
+
+    def __enter__(self):
+        self.append(self._initial_content)
+        return self
+
+    def __exit__(self, *args):
+        os.remove(self.filepath)
+
+        if os.path.exists(self.filepath):
+            raise AssertionError('Failed to delete temporary file: ' + self.filepath)
+
+    def append(self, content):
+        with open(self.filepath, 'a') as f:
+            f.write(content)
